e9635aeec8206fd3b75a63816770e3343472e6e7
admin <123456>
2019-10-16 10:42:18 +0800

    mybatis模板

---
+++ src/main/java/com/demo/Student.java
@@ -0,0 +1,41 @@
+package com.demo;
+
+public class Student {
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private String name;
+    private int age;
+}
